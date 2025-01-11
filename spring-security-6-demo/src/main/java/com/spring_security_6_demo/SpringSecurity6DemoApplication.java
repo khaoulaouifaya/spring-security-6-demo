c50968144cd40142cd0b24d2ee7397e2a3868804
khaoula ouifaya <khaoula.ouifaya@centoria.fr>
2025-01-11 01:21:06 +0100

    first commit

---
+++ spring-security-6-demo/src/main/java/com/spring_security_6_demo/SpringSecurity6DemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring_security_6_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6DemoApplication.class, args);
+	}
+
+}
---
+++ spring-security-6-demo/src/test/java/com/spring_security_6_demo/SpringSecurity6DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security_6_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
